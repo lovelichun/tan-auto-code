7dc989efb31e5b35d5d916cceaea492fc5ed70d0
ZN-yyrj015 <ZN-yyrj015@ZN-yyrj015.fisegk.com>
2017-10-16 14:34:34 +0800

    增加单元测试类

---
+++ src/test/java/test/MyTest.java
@@ -0,0 +1,21 @@
+package test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+
+/**
+ * Created by HuLuo on 2016/8/19.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:spring-context.xml"})
+public class MyTest {
+
+	
+}
